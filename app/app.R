09fb17bf766324ebcc69e0d811e7aeae23e2d0f5
aobermayer4 <aobermayer4@gmail.com>
2024-12-12 13:01:34 -0500

    Added reactive vals

--- app/app.R
+++ app/app.R
@@ -4,22 +4,26 @@ version_id <- paste0("v0.0.9")
 
 # Back end input ---------------------------------------------------------------
 
+Confirmed_Known_HIVNEG_File <- ""
 
+Existing_Known_HIVNEG_File <- ""
 
 # UI ---------------------------------------------------------------------------
 
 ui <-
-  shiny::navbarPage("{ App Name }",
+  shiny::navbarPage("{ SMART }",
                     id = "navbar_id",
                     theme = bslib::bs_theme(bootswatch = "yeti"),
                     ## Homepage ------------------------------------------------
                     shiny::tabPanel("Homepage",
+                                    # Explain SMART acronym what it can be used for - any logos
                     ),
                     ## Screening -----------------------------------------------
                     shiny::tabPanel("Screening",
                                     shiny::sidebarLayout(
                                       shiny::sidebarPanel(
                                         width = 3,
+                                        
                                       ),
                                       shiny::mainPanel(
                                         
@@ -31,6 +35,7 @@ ui <-
                                     shiny::sidebarLayout(
                                       shiny::sidebarPanel(
                                         width = 3,
+                                        
                                       ),
                                       shiny::mainPanel(
                                         
@@ -57,6 +62,25 @@ ui <-
 # Server -----------------------------------------------------------------------
 
 server <- function(input, output, session) {
+  
+  ## Reactive Vals -------------------------------------------------------------
+  input_file <- reactiveVal(NULL)
+  backend_hivneg_file <- reactiveVal(NULL)
+  backend_known_hivneg_file <- reactiveVal(NULL)
+  
+  input_df <- reactiveVal(NULL)
+  input_df_hivpos <- reactiveVal(NULL)
+  input_df_hivneg <- reactiveVal(NULL)
+  
+  backend_hivneg_df <- reactiveVal(NULL)
+  backend_hivneg_mrn <- reactiveVal(NULL)
+  
+  input_hivpos_mrn <- reactiveVal(NULL)
+  input_hivneg_mrn <- reactiveVal(NULL)
+  
+  output_hivpos_mrn <- reactiveVal(NULL)
+  output_hivneg_mrn <- reactiveVal(NULL)
+  hiv_matched_list <- reactiveVal(NULL)
   
   ## Homepage ------------------------------------------------------------------
   
